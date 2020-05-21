bd308cbd8e030132d0ef936957ed96ad46a9e69b
Eberson Oliveira <eberson.olivera@gmail.com>
2020-05-21 19:41:38 -0300

    emitindo mensagens

--- app/src/main/java/br/com/etecmatao/meucursodeandroid/MainActivity.kt
+++ app/src/main/java/br/com/etecmatao/meucursodeandroid/MainActivity.kt
@@ -2,11 +2,18 @@ package br.com.etecmatao.meucursodeandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showMessage(v: View){
+        //Toast é o componente que emite mensagens "simples" ao usuario
+        Toast.makeText(this, getString(R.string.msg_bem_vindo), Toast.LENGTH_SHORT).show()
     }
 }
